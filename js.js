1a2fb17e5272ccfaa1174542a30a8a5b6bc4144a
Harteos7 <harteos14@gmail.com>
2023-12-20 11:06:39 +0100

    time

--- js.js
+++ js.js
@@ -1,12 +1,3 @@
-const titre = document.getElementById('titre');
-titre.textContent = "Bienvenue";
-
-let i = 0;
-while (i < 3) {
-  i++;
-  setTimeout(() => {
-    titre.textContent += " sur le portfolio de Yoann Harmand";
-  }, i * 1000);
-}
-
-console.log(titre);
+setTimeout(() => {
+document.getElementById('test').textContent += " sur le portfolio de Yoann Harmand";
+}, 1000);
