1223c1db63febaf27cbc13f9dace91e7c7e841e9
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2024-06-20 09:57:22 +0200

    + anglais

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var userLang = navigator.language || navigator.userLanguage;
+    userLang = userLang.substring(0, 2); // Get the first two characters of the language code
 
+    var langElements = document.querySelectorAll('.lang-content');
+    langElements.forEach(function(element) {
+        if (element.classList.contains(userLang)) {
+            element.style.display = 'block';
+        }
+    });
+});
