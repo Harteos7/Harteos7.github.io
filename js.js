9eabc4409c636873e421850360f7430aa8d9ba3c
Harteos7 <113110274+Harteos7@users.noreply.github.com>
2024-06-20 10:19:41 +0200

    CV + anglais

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("Le script js.js est chargé et s'exécute correctement.");
     var userLang = navigator.language || navigator.userLanguage;
     userLang = userLang.substring(0, 2); // Get the first two characters of the language code
 
